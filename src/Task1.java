2b7f9c1d15287c76941ce0f7ef9556e38ca78e27
Nataliya Svirid <klybiksvirid@gmail.com>
2021-01-23 21:21:54 +0300

    Commit Task4

--- src/Task1.java
+++ src/Task1.java
@@ -17,9 +17,10 @@ public class Task1 {
             }
         }
         for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++)
-                System.out.print(array[i][j]+" ");
-            System.out.println();
+            for(int j = 0; j < size; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+                System.out.println();
         }
     }
 }
