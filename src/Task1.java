770088c84ff60e46a7fb4f287d02602d8bc864f0
Nataliya Svirid <klybiksvirid@gmail.com>
2021-01-23 21:24:09 +0300

    Commit Task1

--- src/Task1.java
+++ src/Task1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array");
+        System.out.println("Enter your array size");
         int size = sc.nextInt();
         int[][] array = new int[size][size];
         int value = 1;
